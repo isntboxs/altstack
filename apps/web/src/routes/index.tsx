9c6bd16dcc3192b1cad96670c84142c64afce861
mrboxs <rizkywanda200155@gmail.com>
2026-08-06 02:16:08 +0700

    feat: add UI components and implement search and filter section on the index route

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { FilterSection } from '#/components/filter-section'
 import { HeroSection } from '#/components/hero-section'
 
 export const Route = createFileRoute('/')({
@@ -10,6 +11,7 @@ function Home() {
 	return (
 		<>
 			<HeroSection />
+			<FilterSection />
 		</>
 	)
 }
